dc3e5286d67b9fe7e6e8167a6205b2b37bfaab04
xmxe <464817304@qq.com>
2020-07-10 09:13:47 +0800

    thread

---
+++ study-demo/src/main/java/com/xmxe/study_demo/thread/WaitNotify.java
@@ -0,0 +1,40 @@
+package com.xmxe.study_demo.thread;
+
+public class WaitNotify {
+    public static void main(String[] args) {
+        WaitNotify w = new WaitNotify();
+
+        Thread t1 = new Thread(() -> {
+            synchronized (w) {
+                for (int i = 0; i < 25; i++) {
+                    System.out.println(i);
+                    try {
+                        w.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    w.notify();
+                }
+            }
+
+        }, "线程1");
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            synchronized (w) {
+                for (char a = 'a'; a < 'z'; a++) {
+                    System.out.println(a);
+                    w.notify();
+                    try {
+                        w.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        },"线程2");
+        t2.start();
+    }
+
+}
