bc756c5882c89c09eee0cc0f6b18c427dd549747
xmxe <464817304@qq.com>
2020-08-04 16:47:30 +0800

    ThreadLocal实践

---
+++ study-demo/src/main/java/com/xmxe/study_demo/thread/SeqCount.java
@@ -0,0 +1,91 @@
+package com.xmxe.study_demo.thread;
+
+/**
+ * ThreadLocal测试1
+ */
+public class SeqCount {
+    private static ThreadLocal<Integer> seqCount = new ThreadLocal<Integer>() {
+        // 实现initialValue()
+        public Integer initialValue() {
+            return 0;
+        }
+    };
+
+    public int nextSeq() {
+        seqCount.set(seqCount.get() + 1);
+        return seqCount.get();
+    }
+
+    public void removeSeq() {
+        seqCount.remove();
+    }
+
+    public static void main(String[] args) {
+        SeqCount seqCount = new SeqCount();
+        SeqThread thread1 = new SeqThread(seqCount);
+        SeqThread thread2 = new SeqThread(seqCount);
+        SeqThread thread3 = new SeqThread(seqCount);
+        SeqThread thread4 = new SeqThread(seqCount);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+    }
+
+    private static class SeqThread extends Thread {
+        private SeqCount seqCount;
+
+        SeqThread(SeqCount seqCount) {
+            this.seqCount = seqCount;
+        }
+
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println(Thread.currentThread().getName() + " seqCount :" + seqCount.nextSeq());
+            }
+            seqCount.removeSeq();
+        }
+    }
+}
+
+/**
+ * ThreadLocal测试2
+ */
+class ThreadLocalTest {
+    private static ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 100; i++) {
+                    threadLocal.set(i);
+                    System.out.println(Thread.currentThread().getName() + "====" + threadLocal.get());
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } finally {
+                threadLocal.remove();
+            }
+        }, "threadLocal1").start();
+
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println(Thread.currentThread().getName() + "====" + threadLocal.get());
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } finally {
+                threadLocal.remove();
+            }
+        }, "threadLocal2").start();
+    }
+}
