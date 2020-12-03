faf212caec83ed54c1f2de6d6ddaad3857ced692
xmxe <464817304@qq.com>
2020-12-03 16:01:44 +0800

    jdk与cglib动态代理的区别

--- study-demo/src/main/java/com/xmxe/study_demo/proxy/dynamicproxy/CglibProxy.java
+++ study-demo/src/main/java/com/xmxe/study_demo/proxy/dynamicproxy/CglibProxy.java
@@ -4,15 +4,34 @@ import java.lang.reflect.Method;
 
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
+import org.springframework.cglib.proxy.Enhancer;
 
 public class CglibProxy implements MethodInterceptor {
+	/** CGLib需要代理的目标对象 */
+	private Object targetObject;
+	
+	public Object createProxyObject(Object obj) {
+        this.targetObject = obj;
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(obj.getClass());
+        enhancer.setCallback(this);
+        Object proxyObj = enhancer.create();
+        // 返回代理对象
+        return proxyObj;
+    }
+
 
 	@Override
-	public Object intercept(Object object, Method method, Object[] objects, MethodProxy proxy) throws Throwable {
+	public Object intercept(Object proxyObject, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+		Object obj = null;
+		if("buyHouse".equals(method.getName())){
+			//业务逻辑 ...
+			System.out.println("do something...");
+		}
 		System.out.println("Before Method Invoke");
-		proxy.invokeSuper(object, objects);
+		obj = method.invoke(targetObject, objects);
 		System.out.println("After Method Invoke");
-		return object;
+		return obj;
 	}
 
 }
--- study-demo/src/main/java/com/xmxe/study_demo/proxy/dynamicproxy/DynamicProxtTest.java
+++ study-demo/src/main/java/com/xmxe/study_demo/proxy/dynamicproxy/DynamicProxtTest.java
@@ -1,8 +1,5 @@
 package com.xmxe.study_demo.proxy.dynamicproxy;
 
-import java.lang.reflect.Proxy;
-
-import org.springframework.cglib.proxy.Enhancer;
 
 public class DynamicProxtTest {
     public static void main(String[] args) {
@@ -11,31 +8,19 @@ public class DynamicProxtTest {
     }
 
 	public static void jdkDynamicProxy() {
-		BuyHouse buyHouse = new BuyHouseImpl();
-		BuyHouse proxyBuyHouse = (BuyHouse) Proxy.newProxyInstance(BuyHouse.class.getClassLoader(),
-				new Class[] { BuyHouse.class }, new JDKProxy(buyHouse));
+		JDKProxy jdkProxy = new JDKProxy();
+		// 创建代理对象
+		BuyHouse proxyBuyHouse = (BuyHouse)jdkProxy.newProxy(new BuyHouseImpl());
 		proxyBuyHouse.buyHouse();
     }
     
     public static void cglibDynamicProxy() {
-		CglibProxy daoProxy = new CglibProxy();
-		Enhancer enhancer = new Enhancer();
-		enhancer.setSuperclass(Dao.class);
-		enhancer.setCallback(daoProxy);
-		Dao dao = (Dao) enhancer.create();
-		dao.update();
-		dao.select();
+		CglibProxy cglibProxy = new CglibProxy();
+		BuyHouse buyHouse = (BuyHouse) cglibProxy.createProxyObject(new BuyHouseImpl());
+		buyHouse.buyHouse();
 	}
 }
-class Dao {
-	public void update() {
-        System.out.println("PeopleDao.update()");
-    }
-    
-    public void select() {
-        System.out.println("PeopleDao.select()");
-    }
-}
+
 class BuyHouseImpl implements BuyHouse {
 
 	@Override
@@ -50,4 +35,12 @@ interface BuyHouse {
 }
 /**
  * 终于有人把 java代理 讲清楚了，万字详解！(https://mp.weixin.qq.com/s/C9Vpfcgl3NB_0rBpLh2yCA)
+ * 【Spring基础】JDK和CGLIB动态代理区别(https://blog.csdn.net/yhl_jxy/article/details/80635012)
+ * 
+ * JDK代理是不需要第三方库支持，只需要JDK环境就可以进行代理，使用条件:
+ * 1）实现InvocationHandler 
+ * 2）使用Proxy.newProxyInstance产生代理对象
+ * 3）被代理的对象必须要实现接口
+ * CGLib必须依赖于CGLib的类库，但是它需要类来实现任何接口代理的是指定的类生成一个子类，
+ * 覆盖其中的方法，是一种继承但是针对接口编程的环境下推荐使用JDK的代理；
  */
