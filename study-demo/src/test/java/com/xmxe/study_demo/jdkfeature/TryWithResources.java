8decb0ff842b2452ab5942bdd3f62af6ea791f8e
xmxe <464817304@qq.com>
2020-12-09 17:21:45 +0800

    局部内部类

--- study-demo/src/test/java/com/xmxe/study_demo/jdkfeature/TryWithResources.java
+++ study-demo/src/test/java/com/xmxe/study_demo/jdkfeature/TryWithResources.java
@@ -2,23 +2,23 @@ package com.xmxe.study_demo.jdkfeature;
 
 public class TryWithResources {
    public static void main(String[] args) {
-       try(TWR t = new TWR("a")){
+       //实现try-with-resources必须实现AutoCloseable接口
+        class TWR implements AutoCloseable{
 
-       }catch(Exception e){
-           e.printStackTrace();
-       }
+            private String str;
+            public TWR(String str){this.str = str;}
+            @Override
+            public void close() throws Exception {
+                System.out.println(str.toUpperCase());
+            }
+            
+        }
+        try(TWR t = new TWR("a")){
+            int a = 1 / 0;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
    }
 
-    
-}
-//实现try-with-resources必须实现AutoCloseable接口
-class TWR implements AutoCloseable{
-
-    private String str;
-    public TWR(String str){this.str = str;}
-    @Override
-    public void close() throws Exception {
-        System.out.println(str.toUpperCase());
-    }
     
 }
