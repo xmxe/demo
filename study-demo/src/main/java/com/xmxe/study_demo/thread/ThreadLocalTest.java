88f42842b0d9198937f93354e15c32b74ef4900a
xmxe <464817304@qq.com>
2021-01-07 09:08:09 +0800

    InheritableThreadLocal

--- study-demo/src/main/java/com/xmxe/study_demo/thread/ThreadLocalTest.java
+++ study-demo/src/main/java/com/xmxe/study_demo/thread/ThreadLocalTest.java
@@ -112,3 +112,17 @@ class ThreadLocalTest3{
     }
        
 }
+/**
+ * InheritableThreadLocal
+ * 父线程传递本地变量到子线程的java类
+ */
+class InheritableThreadLocalTest {
+    public static void main(String[] args) {
+        InheritableThreadLocal<String> inheritableThreadLocal = new InheritableThreadLocal<>();
+        inheritableThreadLocal.set("主线程变量");
+        new Thread(()->{
+            System.out.println(inheritableThreadLocal.get());
+        }).start();
+        
+    }
+}
