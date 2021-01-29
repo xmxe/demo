87243981c6dddef73e3382ff234766414bf8f8fd
xmxe <464817304@qq.com>
2021-01-29 10:25:14 +0800

    socket refactor

--- study-demo/src/main/java/com/xmxe/study_demo/socket/SocketClient.java
+++ study-demo/src/main/java/com/xmxe/study_demo/socket/SocketClient.java
@@ -7,49 +7,59 @@ import java.net.Socket;
 public class SocketClient {
 
 	public static void main(String[] args) throws Exception {
-		// ---基础模式 begin---
+		// 基础模式
+		basicModeSocketClient();
+		// 双向通讯
+		twoWayCommunicationClient();
+	}
+
+	/**
+	 * 基础模式
+	 */
+	public static void basicModeSocketClient() throws Exception{
 		// 要连接的服务端IP地址和端口
-	    String host = "127.0.0.1"; 
-	    int port = 55533;
-	    // 与服务端建立连接
-	    Socket socket = new Socket(host, port);
-	    // 建立连接后获得输出流
-	    OutputStream outputStream = socket.getOutputStream();
-	    String message="你好123";
-	    socket.getOutputStream().write(message.getBytes("UTF-8"));
-	    outputStream.close();
-	    socket.close();
-	   // ---基础模式 end---
-	    
+		String host = "127.0.0.1"; 
+		int port = 55533;
+		// 与服务端建立连接
+		Socket socket = new Socket(host, port);
+		// 建立连接后获得输出流
+		OutputStream outputStream = socket.getOutputStream();
+		String message="你好  yiwangzhibujian";
+		socket.getOutputStream().write(message.getBytes("UTF-8"));
+		outputStream.close();
+		socket.close();
+	}
+	/**
+	 * 双向通信，发送消息并接受消息
+	 * 与之前server的不同在于，当读取完客户端的消息后，打开输出流，将指定消息发送回客户端
+	 */
+	public static void twoWayCommunicationClient() throws Exception{
+		// 要连接的服务端IP地址和端口
+		String host = "127.0.0.1";
+		int port = 55533;
+		// 与服务端建立连接
+		Socket socket = new Socket(host, port);
+		// 建立连接后获得输出流
+		OutputStream outputStream = socket.getOutputStream();
+		String message = "你好  yiwangzhibujian";
+		outputStream.write(message.getBytes("UTF-8"));
+		//通过shutdownOutput告诉服务器已经发送完数据，后续只能接受数据
+		socket.shutdownOutput();
 		
-		/* 双向通信，发送消息并接受消息 start
-		// 要连接的服务端IP地址和端口
-	    String host = "127.0.0.1";
-	    int port = 55533;
-	    // 与服务端建立连接
-	    Socket socket = new Socket(host, port);
-	    // 建立连接后获得输出流
-	    OutputStream outputStream = socket.getOutputStream();
-	    String message = "你好 234";
-	    socket.getOutputStream().write(message.getBytes("UTF-8"));
-	    //通过shutdownOutput告诉服务器已经发送完数据，后续只能接受数据
-	    socket.shutdownOutput();
-	    
-	    InputStream inputStream = socket.getInputStream();
-	    byte[] bytes = new byte[1024];
-	    int len;
-	    StringBuilder sb = new StringBuilder();
-	    while ((len = inputStream.read(bytes)) != -1) {
-	      //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
-	      sb.append(new String(bytes, 0, len,"UTF-8"));
-	    }
-	    System.out.println("get message from server: " + sb);
-	    
-	    inputStream.close();
-	    outputStream.close();
-	    socket.close();
-	    //双向通信，发送消息并接受消息 end
-	    */
+		InputStream inputStream = socket.getInputStream();
+		byte[] bytes = new byte[1024];
+		int len;
+		StringBuilder sb = new StringBuilder();
+		while ((len = inputStream.read(bytes)) != -1) {
+		  //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
+		  sb.append(new String(bytes, 0, len,"UTF-8"));
+		}
+		System.out.println("get message from server: " + sb);
+		
+		inputStream.close();
+		outputStream.close();
+		socket.close();
+	
 	}
 
 }
