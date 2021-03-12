266db9fb1a2ac57d47485e68f417d191299bd4d7
xmxe <464817304@qq.com>
2021-03-12 14:40:07 +0800

    atomicInteger方法详解

--- study-demo/src/main/java/com/xmxe/study_demo/thread/juc/LimitingByAomicInteger.java
+++ study-demo/src/main/java/com/xmxe/study_demo/thread/juc/LimitingByAomicInteger.java
@@ -10,14 +10,15 @@ import java.util.concurrent.atomic.LongAdder;
  * 构造方法
  * public AtomicInteger(int initialValue)   创建具有给定初始值的新 AtomicInteger。参数：initialValue - 初始值
  * public AtomicInteger()  创建具有初始值0 的新 AtomicInteger。
- * 方法详细
+ * 
  * public final int get()  获取当前值。返回：当前值
  * public final void set(int newValue)  设置为给定值。参数：newValue - 新值
- * public final void lazySet(int newValue)   最后设置为给定值。参数：newValue - 新值
+ * public final void lazySet(int newValue) 不使用内存屏障设置 最后设置为给定值。参数：newValue - 新值
+ * 
  * 从以下版本开始：1.6 
  * public final int getAndSet(int newValue)   以原子方式设置为给定值，并返回旧值。参数：newValue - 新值 返回：以前的值
- * public final boolean compareAndSet(int expect, int update) 如果当前值 == 预期值，则以原子方式将该值设置为给定的更新值。参数：expect - 预期值   update - 新值 返回：如果成功，则返回 true。返回 False 指示实际值与预期值不相等。
- * public final boolean weakCompareAndSet(int expect,  int update) 如果当前值 == 预期值，则以原子方式将该设置为给定的更新值。可能意外失败并且不提供排序保证，所以只有在很少的情况下才对compareAndSet 进行适当地选择。 参数：expect - 预期值   update - 新值 返回：如果成功，则返回 true。
+ * public final boolean compareAndSet(int expect, int update)  CAS实现 比较并交换 一种不依靠锁实现并发安全的技术 如果当前值 == 预期值，则以原子方式将该值设置为给定的更新值。参数：expect - 预期值   update - 新值 返回：如果成功，则返回 true。返回 False 指示实际值与预期值不相等。
+ * public final boolean weakCompareAndSet(int expect,  int update) 与compareAndSet实现一样 只是针对不同平台的架构 如果当前值 == 预期值，则以原子方式将该设置为给定的更新值。可能意外失败并且不提供排序保证，所以只有在很少的情况下才对compareAndSet 进行适当地选择。 参数：expect - 预期值   update - 新值 返回：如果成功，则返回 true。
  * public final int getAndIncrement()   以原子方式将当前值加 1。返回：以前的值
  * public final int getAndDecrement()    以原子方式将当前值减 1。返回：以前的值
  * public final int getAndAdd(int delta)    以原子方式将给定值与当前值相加。参数：delta - 要加上的值 返回：以前的值
@@ -29,7 +30,7 @@ import java.util.concurrent.atomic.LongAdder;
  * public long longValue()   从类Number 复制的描述 以 long 形式返回指定的数值。这可能涉及到舍入或取整。指定者：类 Number 中的longValue 返回：转换为 long 类型后该对象表示的数值。
  * public float floatValue() 从类 Number 复制的描述 以 float 形式返回指定的数值。这可能会涉及到舍入。指定者：类 Number 中的floatValue 返回：转换为 float 类型后该对象表示的数值。
  * public double doubleValue() 从类 Number 复制的描述 以 double 形式返回指定的数值。这可能会涉及到舍入。 指定者：类 Number 中的doubleValue 返回：转换为 double 类型后该对象表示的数值。
- */
+ **/
 public class LimitingByAomicInteger {
 
     /**
