048a725e8a3c5ad01d92b2d51723c26f130e43fd
xmxe <464817304@qq.com>
2021-08-02 08:55:08 +0800

    适配器模式

--- study-demo/src/main/java/com/xmxe/study_demo/designpattern/adapter/AdapterModel.java
+++ study-demo/src/main/java/com/xmxe/study_demo/designpattern/adapter/AdapterModel.java
@@ -4,7 +4,7 @@ package com.xmxe.study_demo.designpattern.adapter;
  * 适配器模式,就是将一个类的接口转换成客户希望的另一个接口，使接口不兼容的类可以一起工作，也被称为包装器模式（Wrapper）。
  * 巧妙的运用适配器模式，让你的工作量至少减轻一半 https://mp.weixin.qq.com/s/IRCnLajfpHU5bQCo9HQxUg
  */
-public class PlayerClient {
+public class AdapterModel {
     public static void main(String[] args) {
         //对象适配
         ExpensiveAdapter adapter = new ExpensiveAdapter(new HuaweiPhone());
@@ -16,11 +16,6 @@ public class PlayerClient {
 interface Phone {
     void productPhone();
 }
-
-interface Player{
-    void action();
-}
-
 class HuaweiPhone implements Phone{
 
     @Override
@@ -28,6 +23,9 @@ class HuaweiPhone implements Phone{
         System.out.println("生产一部华为手机");        
     }
     
+}
+interface Player{
+    void action();
 }
 class ExpensiveAdapter implements Player{
 
