9a47f0dcaec2049ac2f1169bbc5117886ba5c1f7
xmxe <464817304@qq.com>
2023-03-09 08:47:13 +0800

    stream&优雅下线

--- study-demo/src/main/java/com/xmxe/study_demo/jdkfeature/StreamTest.java
+++ study-demo/src/main/java/com/xmxe/study_demo/jdkfeature/StreamTest.java
@@ -2,6 +2,7 @@ package com.xmxe.study_demo.jdkfeature;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
@@ -38,33 +39,28 @@ public class StreamTest {
          * Stream操作往往可以通过顺序或者并行两种方式来执行。
          */
         Student s1 = new Student(1L, "a", 15, "浙江");
-        Student s2 = new Student(2L, "b", 15, "湖北");
-        Student s3 = new Student(3L, "c", 17, "北京");
-        Student s4 = new Student(4L, "d", 17, "浙江");
-        List<Student> students = List.of(s1,s2,s3,s4);
-
-        //构造Stream流的方式
-        Stream.of("Monkey", "Lion", "Giraffe", "Lemur").mapToInt(String::length).forEach(System.out::println);
-        Stream<String> stream = Stream.of("a", "b", "c");
+        List<Student> students = List.of(s1);
+        List<String> list = List.of("a","b","c");
+        List<Integer> number = List.of(1,2,3,4);
         String[] strArray = new String[] { "a", "b", "c" };
-        stream = Stream.of(strArray);
-        stream = Arrays.stream(strArray);
-        List<String> list = Arrays.asList(strArray);
-        stream = list.stream();
-        //注意:一个Stream流只可以使用一次，上面代码为了简洁而重复使用了数次，因此会抛出 stream has already been operated upon or closed 异常。
 
-        //Stream流的之间的转换
+        // 构造Stream流的方式
+        Stream<String> stream = Stream.of("a", "b", "c");
+        Stream<String> stream1 = Stream.of(strArray);
+        Stream<String> stream2 = Arrays.stream(strArray);
+        Stream<String> stream3 = list.stream();
+        // 注意:一个Stream流只可以使用一次，重复使用了多次会抛出stream has already been operated upon or closed异常。
+
+        // Stream流的之间的转换
         try {
-            // stream转换成 Array
-            String[] strArray1 = stream.toArray(String[]::new);
-
-            // stream转换成 Collection
-            List<String> list1 = stream.collect(Collectors.toList());
-            List<String> list2 = stream.collect(Collectors.toCollection(ArrayList::new));
-            Set<String> set = stream.collect(Collectors.toSet());
-            Stack<String> stack = stream.collect(Collectors.toCollection(Stack::new));
-
-            // 转换成 String
+            // stream转换成Array
+            String[] strArray1 = stream2.toArray(String[]::new);
+            // stream转换成Collection
+            List<String> list1 = stream3.collect(Collectors.toList());
+            List<String> list2 = stream3.collect(Collectors.toCollection(ArrayList::new));
+            Set<String> set = stream3.collect(Collectors.toSet());
+            Stack<String> stack = stream3.collect(Collectors.toCollection(Stack::new));
+            // 转换成String
             String str = stream.collect(Collectors.joining()).toString();
         } catch (Exception e) {
             e.printStackTrace();
@@ -75,29 +71,20 @@ public class StreamTest {
          * stream是顺序流，由主线程按顺序对流执行操作，而parallelStream是并行流，内部以多线程并行执行的方式对流进行操作，但前提是流中的数据处理没有顺序要求
          * 如果流中的数据量足够大，并行流可以加快处速度。除了直接创建并行流，还可以通过parallel()把顺序流转换成并行流
          */
-        //parallelStream 是流并行处理程序的代替方法。
-        List<String> strings = Arrays.asList("a", "", "c", "", "e","", " ");
+        // parallelStream是流并行处理程序的代替方法。
         // 获取空字符串的数量
-        long count = strings.parallelStream().filter(string -> string.isEmpty()).count();
+        long count = list.parallelStream().filter(string -> string.isEmpty()).count();
 
-        // filter（筛选）
+        // filter(筛选)
         List<Student> streamStudents = students.stream().filter(s -> "浙江".equals(s.getAddress())).collect(Collectors.toList());
-        streamStudents.forEach(System.out::println);
         Student stuOrelse = students.stream().filter(s -> "李四".equals(s.getName())).findAny().orElse(s1);
         int sum = students.stream().filter(u -> "张三".equals(u.getName())).mapToInt(u -> u.getAge()).sum();
 
         // distinct(去重)
-        strings.stream().distinct().forEach(System.out::println);
-        students.stream().distinct().forEach(System.out::println);
-        List<String> wnums = strings.stream().flatMap(line -> Stream.of(line.split(""))).filter(word -> word.length() >= 0)
-            .map(String::toLowerCase).distinct().sorted().collect(Collectors.toList());
+        list.stream().distinct().forEach(System.out::println);
+        List<String> wnums = list.stream().flatMap(line -> Stream.of(line.split(""))).filter(word -> word.length() >= 0).map(String::toLowerCase).distinct().sorted().collect(Collectors.toList());
 
-        // sorted(排序)
-        list.stream().sorted().forEach(System.out::println);
-        students.stream().sorted((stu1, stu2) -> Long.compare(stu2.getId(), stu1.getId()))
-                .sorted((stu1, stu2) -> Integer.compare(stu2.getAge(), stu1.getAge())).forEach(System.out::println);
-
-        // limit（限制返回个数）
+        // limit(限制返回个数)
         list.stream().limit(2).forEach(System.out::println);
 
         // skip(删除元素)
@@ -106,37 +93,36 @@ public class StreamTest {
         // reduce(聚合)
         List<String> list_reduce = Arrays.asList("欢", "迎", "你");
         String appendStr = list_reduce.stream().reduce("北京", (a, b) -> a + b);
-        String concat = Stream.of("A", "B", "C", "D").reduce("", String::concat);//拼接字符串
+        String concat = stream.reduce("", String::concat);//拼接字符串->abc
         double minValue = Stream.of(-4.0, 1.0, 3.0, -2.0).reduce(Double.MAX_VALUE, Double::min);//得到最小值
-        int sumValue = Stream.of(1, 2, 3, 4).reduce(Integer::sum).get();//求和, 无起始值
-        sumValue = Stream.of(1, 2, 3, 4).reduce(1, Integer::sum);//求和, 有起始值
-        concat = Stream.of("a", "B", "c", "D", "e", "F").filter(x -> x.compareTo("Z") > 0).reduce("", String::concat);//过滤拼接字符串
-
-        //iterate与reduce很像 使用时管道必须有limit这样的操作来限制Stream大小。
+        int sumValue = number.stream().reduce(Integer::sum).get();//求和, 无起始值
+        int sumValue1 = number.stream().reduce(1, Integer::sum);//求和, 有起始值
+        String concat1 = stream.filter(x -> x.compareTo("Z") > 0).reduce("", String::concat);//过滤拼接字符串   
+        // stream求集合最大值最小值
+        Integer max = number.stream().reduce(Integer::max).get();
+        Integer min = number.stream().reduce(Integer::min).get();
+        
+        // iterate与reduce很像 使用时管道必须有limit这样的操作来限制Stream大小。
         Stream.iterate(2, n -> n + 2).limit(5).forEach(x -> System.out.print(x + " "));//从2开始生成一个等差数列
 
-        //通过实现Supplier类的方法可以自定义流计算规则。
-        //随机获取两条学生信息
+        // 通过实现Supplier类的方法可以自定义流计算规则。
+        // 随机获取两条学生信息
         class UserSupplier implements Supplier<Student> {
             private int index = 10;
             private Random random = new Random();
 
             @Override
             public Student get() {
-            return new Student(index++,"pancm" + random.nextInt(10));
+                return new Student(index++,"pancm" + random.nextInt(10));
             }
         }
         Stream.generate(new UserSupplier()).limit(2).forEach(u -> System.out.println(u.getId() + ", " + u.getName()));
 
-        //summaryStatistics使用
-        //得到最大、最小、之和以及平均数。
-        List<Integer> numbers = Arrays.asList(1, 5, 7, 3, 9);
-        IntSummaryStatistics stats = numbers.stream().mapToInt((x) -> x).summaryStatistics();
-
-        System.out.println("列表中最大的数 : " + stats.getMax());
-        System.out.println("列表中最小的数 : " + stats.getMin());
-        System.out.println("所有数之和 : " + stats.getSum());
-        System.out.println("平均数 : " + stats.getAverage());
+        // summaryStatistics使用
+        // 得到最大、最小、之和以及平均数。
+        IntSummaryStatistics stats = number.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("列表中最大的数 : " + stats.getMax() + "列表中最小的数 : " + stats.getMin()
+        + "所有数之和 : " + stats.getSum() + "平均数 : " + stats.getAverage());
 
         // min() max()
         Student minS = students.stream().min((stu1, stu2) -> Integer.compare(stu1.getAge(), stu2.getAge())).get();
@@ -144,9 +130,9 @@ public class StreamTest {
         int maxLines = list.stream().mapToInt(String::length).max().getAsInt();
         int minLines = list.stream().mapToInt(String::length).min().getAsInt();
 
-        //peek对每个元素执行操作并返回一个新的Stream,如同于map，能得到流中的每一个元素。但map接收的是一个Function表达式，有返回值；而peek接收的是Consumer表达式，没有返回值
-        Stream.of("one", "two", "three", "four").filter(e -> e.length() > 3).peek(e -> System.out.println("转换之前: " + e))
-            .map(String::toUpperCase).peek(e -> System.out.println("转换之后: " + e)).collect(Collectors.toList());
+        // 同map操作,但是使用peek操作流,流中的元素不会改变,能得到流中的每一个元素。map会改变流中的元素,map接收的是一个Function表达式,有返回值,而peek接收的是Consumer表达式,没有返回值
+        list.stream().filter(e -> e.length() > 3)
+        .peek(e -> System.out.println("转换之前: " + e)).map(String::toUpperCase).peek(e -> System.out.println("转换之后: " + e)).collect(Collectors.toList());
 
         /**
          * anyMatch/allMatch/noneMatch（匹配）
@@ -172,9 +158,7 @@ public class StreamTest {
          * Optional.ifPresent(Consumer<? super T> consumer) 表示当Optional包裹的元素不为空时，执行consumer
          */
         num.stream().filter(n -> n > 2).findAny().ifPresent(System.out::println);
-        //stream求集合最大值最小值
-        num.stream().reduce(Integer::max);
-        num.stream().reduce(Integer::min);
+        
 
         // map(转换)用于映射每个元素到对应的结果，一对一。
         List<String> addresses = students.stream().map(s -> "住址:" + s.getAddress()).collect(Collectors.toList());
@@ -197,7 +181,6 @@ public class StreamTest {
      */
     public void sort(){
         List<String> sList = List.of("t","a","4","yu");
-
         // 1.使用Stream sorted()完成自然排序、比较器和反向排序
         // sorted(Comparator<? super T> comparator)：这里我们使用lambda表达式创建一个Comparator实例。
         // 我们可以按升序和降序对流元素进行排序。
@@ -208,15 +191,28 @@ public class StreamTest {
         sList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
 
         List<Student> studentList = List.of(new Student(1,"name1"),new Student(2,"name2"));
+        // sorted(排序)
+        studentList.stream().sorted((stu1, stu2) -> Long.compare(stu2.getId(), stu1.getId())).sorted((stu1, stu2) -> Integer.compare(stu2.getAge(), stu1.getAge())).forEach(System.out::println);
         studentList.stream().sorted(Comparator.comparing(Student::getName)).forEach(System.out::println);
         // 反转
         studentList.stream().sorted(Comparator.comparing(Student::getAge).reversed()).forEach(System.out::println);
+        // 并行排序
+        studentList.parallelStream().sorted(Comparator.comparing(Student::getAge)).collect(Collectors.toList());
 
         Map<String, String> map = Map.of("k1", "v1","k2","v2");
         map.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getValue))
             .forEach(e -> System.out.println("Key: "+ e.getKey() +", Value: "+ e.getValue()));
         map.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey))
             .forEach(e -> System.out.println("Key: "+ e.getKey() +", Value: "+ e.getValue()));
+
+        // 2.不使用stream 直接使用list.sort()方法排序
+        studentList.sort((u1, u2) -> u1.getAge() - u2.getAge());
+        studentList.sort(Comparator.comparing(Student::getAge));
+        studentList.sort(Comparator.comparing(Student::getAge).reversed());
+        // 多次排序
+        studentList.sort(Comparator.comparing(Student::getAge).thenComparing(Student::getName));
+        // Collections排序
+        Collections.sort(studentList, Comparator.comparing(Student::getAge));
 
     }
 
@@ -233,6 +229,8 @@ public class StreamTest {
         // 根据Map的key排序 创建一个linkedhashmap 遍历时有序
         Map<Long, List<Map<String, Object>>> sortedmap = new LinkedHashMap<>();
         groupBy.entrySet().stream().sorted(Map.Entry.<Long, List<Map<String,Object>>>comparingByKey()).forEachOrdered(e->sortedmap.put(e.getKey(),e.getValue()));
+        // 降序
+        // groupBy.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))...
         sortedmap.forEach((k,v)->{
             Map<String,Object> m = new HashMap<>();
             m.put("time",new Date(k));
@@ -248,7 +246,6 @@ public class StreamTest {
         // Map<String, List<User>> groupMap = userList.stream().collect(Collectors.groupingBy(User::getCity));
         // 多属性分组(根据用户的城市和性别分组)
         // Map<String, List<User>> groupMap = userList.stream().collect(Collectors.groupingBy(u -> u.getCity() + "|" + u.getSex()));
-
         class UserSupplier2 implements Supplier<Student> {
             private int index = 10;
             private Random random = new Random();
@@ -265,11 +262,14 @@ public class StreamTest {
             Map.Entry<Integer, List<Student>> persons =  it.next();
             System.out.println("id " + persons.getKey() + " = " + persons.getValue());
         }
-
         // 通过id进行分组排序:
         // id 10 = [{"id":10,"name":"pancm1"}]
         // id 11 = [{"id":11,"name":"pancm3"}, {"id":11,"name":"pancm6"}, {"id":11,"name":"pancm4"}, {"id":11,"name":"pancm7"}]
-
+       
+       
+        /**
+         * 分区排序
+         */
         class UserSupplier3 implements Supplier<Student> {
             private int index = 16;
             private Random random = new Random();
@@ -279,11 +279,9 @@ public class StreamTest {
              return new Student( index++,"pancm" + random.nextInt(10));
             }
         }
-
         System.out.println("通过年龄进行分区排序:");
         Map<Boolean, List<Student>> children = Stream.generate(new UserSupplier3()).limit(5)
             .collect(Collectors.partitioningBy(p -> p.getId() < 18));
-
         System.out.println("小孩: " + children.get(true));
         System.out.println("成年人: " + children.get(false));
 
