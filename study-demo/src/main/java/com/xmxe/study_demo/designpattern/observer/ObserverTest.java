95cddfe95d12156350a70238852aed931e6aef2c
xmxe <464817304@qq.com>
2023-04-14 09:07:41 +0800

    万能的导出excel工具

--- study-demo/src/main/java/com/xmxe/study_demo/designpattern/observer/ObserverTest.java
+++ study-demo/src/main/java/com/xmxe/study_demo/designpattern/observer/ObserverTest.java
@@ -1,8 +1,8 @@
 package com.xmxe.study_demo.designpattern.observer;
 
-// import java.util.Observable;
-// import java.util.Observable;
-// import java.util.Observer;
+import java.util.Observable;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * jdk9之后过时 原因1、不能序列化 2、不是线程安全 3、支持事件模型的功能简单
@@ -11,63 +11,62 @@ package com.xmxe.study_demo.designpattern.observer;
  * 不可序列化-因为，Observable不实现可序列化。因此，您既不能序列化Observable也不能对其子类进行序列化。
  * 没有线程安全-方法可以被其子类覆盖，并且事件通知可以以不同的顺序发生，并且可能在不同的线程上发生，这足以破坏任何“线程安全”。
  * 并且大多数问题都具有修复的复杂性，但仍然“未解决”或没有积极的发展，这就是不推荐使用的原因。
- * 观察者模式的替代方案：对于更丰富的事件模型，请考虑使用该java.beans程序包。为了在线程之间进行可靠且有序的消息传递，请考虑
- * 使用java.util.concurrent程序包中的并发数据结构之一有关反应式流样式的编程，请参见Flow API。
+ * 观察者模式的替代方案：对于更丰富的事件模型，请考虑使用该java.beans程序包。为了在线程之间进行可靠且有序的消息传递，请考虑使用java.util.concurrent程序包中的并发数据结构之一有关反应式流样式的编程，请参见Flow API。
  */
 
 public class ObserverTest {
     public static void main(String[] args) {
 
-        // TheObserved theObserved = new TheObserved();
-        // ObserverImpl observerImpl1 = new ObserverImpl("1");
-        // ObserverImpl observerImpl2 = new ObserverImpl("2");
+        TheObserved theObserved = new TheObserved();
+        ObserverImpl observerImpl1 = new ObserverImpl("1");
+        ObserverImpl observerImpl2 = new ObserverImpl("2");
 
-        // theObserved.addObserver(observerImpl1);
-        // theObserved.addObserver(observerImpl2);
+        theObserved.addObserver(observerImpl1);
+        theObserved.addObserver(observerImpl2);
 
-        // theObserved.eat();
+        theObserved.eat();
     }
 
     /**
      * 观察者模式：被观察者 当此类发生变化进行通知
      */
-    // static class TheObserved extends Observable{
+    static class TheObserved extends Observable{
 
         /**
          * 定义一个被观察者方法 当执行这个方法通知到观察者
          */
-        // public void eat(){
-        //     System.out.println("开始执行eat方法,准备通知观察者...");
-        //     // 改变状态
-        //     this.setChanged();
-        //     // 通知所有观察者
-        //     this.notifyObservers("this is args");
-        // }
+        public void eat(){
+            System.out.println("开始执行eat方法,准备通知观察者...");
+            // 改变状态
+            this.setChanged();
+            // 通知所有观察者
+            this.notifyObservers("this is args");
+        }
 
         /**
          * 当执行eat方法时 在观察者回调方法里面调用此方法
          *
          */
-    //     public void noodles(Object args){
-    //         System.out.println(args);
-    //     }
-    // }
+        public void noodles(Object args){
+            System.out.println(args);
+        }
+    }
 
     /**
      * 观察者模式 ：观察者
      */
-    // static class ObserverImpl implements Observer{
+    static class ObserverImpl implements Observer{
 
-    //     private String name;
+        private String name;
 
-    //     public ObserverImpl(String name){this.name = name;}
+        public ObserverImpl(String name){this.name = name;}
 
-    //     public String getName(){
-    //         return name;
-    //     }
-    //     public void setName(String name){
-    //         this.name = name;
-    //     }
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
 
         /**
          * 当被观察者发生改变通知时执行此方法
@@ -75,13 +74,13 @@ public class ObserverTest {
          * @param arg 调用notifyObservers方法时传递的参数
          */
         
-    //     @Override
-    //     public void update(Observable o, Object arg) {
-    //         TheObserved theObserved = (TheObserved)o;
-    //         theObserved.noodles(String.format("%s----%s",arg,name));
-    //     }
+        @Override
+        public void update(Observable o, Object arg) {
+            TheObserved theObserved = (TheObserved)o;
+            theObserved.noodles(String.format("%s----%s",arg,name));
+        }
         
-    // }
+    }
 }
 
 
